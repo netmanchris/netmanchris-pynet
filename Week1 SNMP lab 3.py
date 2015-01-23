cd4e72093d0137fe3cf530114edcf68bb299defc
netmanchris <chris_young@me.com>
2015-01-22 23:29:44 -0500

    Updated as per notes
    Made some adjustments as per feedback. Not done yet.

--- Week1 SNMP lab 3.py
+++ Week1 SNMP lab 3.py
@@ -84,41 +84,62 @@ def snmp_extract(snmp_data):
 
 
 def gather_devices():
-    global list_of_devices
-    # Function to gather user input for a list of IP addresses and SNMP strings
-    list_of_devices = []
+    ''' Gathers user input of IP_ADDRESS and SNMP_STRING and appends them as a dictionary
+    into the DEV_LIST list.'''
+    global DEV_LIST
+
+    #initializes function variables
+    DEV_LIST = []
     more = ('''Y''')
+    
     while more == "Y" or more == "y":
-        #gathers user input to populate list_of_devices list of dictionies
-        #loops until user answers 'N'
-        device=input('''What is the IP address of the device you wish to connect to : ''')
-        SNMP_String=input('''What is the SNMP read String for this device?: ''')
-        device_dict = {'DeviceIP':device,'String':SNMP_String}
-        list_of_devices.append(device_dict)
+        '''gathers user input to populate list_of_devices list of dictionies
+        loops until user answers 'N''''
+        IP_ADDRESS=input('''What is the IP address of the device you wish to connect to : ''')
+        SNMP_STRING=input('''What is the SNMP read String for this device?: ''')
+
+        #creates dictionary DEV_DICT from IP_ADDRESS and SNMP_STRING VARs.
+        DEV_DICT = {'DeviceIP':IP_ADDRESS,'String':SNMP_STRING}
+
+        #appends DEV_DICT
+        DEV_LIST.append(DEV_DICT)
         more = input('''Do you wish to add more devices? Y/N: ''')
     else:
-        return list_of_devices
+        return DEV_LIST
       
 
-def lab3(list=None):
-    #takes the list generated from the gather_devices function and iterates over
-    #the list using the SNMP String and IP address as inputs to gather the sysname 
-    #and sysdesc over SNMP. Prints out the sysdesc and sysname for each item in the list
-    if list == None:
-        list = list_of_devices
+def lab3(get_snmp_info=None):
+    '''Takes the list generated from the gather_devices function and iterates over
+    the list using the SNMP String and IP address as inputs to gather the sysname 
+    and sysdesc over SNMP. Prints out the sysdesc and sysname for each item in the list'''
+
+
+    #sets get_snmp_info to global DEV_LIST generated from gather_devices() function
+    if get_snmp_info == None:
+        get_snmp_info = DEV_LIST
+
+        
     print("\n------------------------------------------------------------------------------------------\n\n")
-    for i in list:
-        COMMUNITY_STRING = i['String']
-        IP = i['DeviceIP']
+
+    #Function 
+    for DEVICE in DEV_LIST:
+        #sets function variables
+         IP_ADDRESS = DEVICE['DeviceIP']
+        COMMUNITY_STRING = DEVICE['String']
         SNMP_PORT = 161
+
+        #creates a combined variable from 
         a_device = (IP, COMMUNITY_STRING, SNMP_PORT)
-        sysnameoid = ".1.3.6.1.2.1.1.5.0"
-        sysdescoid = ".1.3.6.1.2.1.1.1.0"
-        snmp_sysname = (snmp_get_oid(a_device, oid=sysnameoid))
-        sysname = snmp_extract(snmp_sysname)
-        snmp_sysdesc = (snmp_get_oid(a_device, oid=sysdescoid))
-        sysdesc = snmp_extract(snmp_sysdesc)
-        if "Cisco" in sysdesc:
+
+        #This section sets the SNMP SYSOIDS we are going to use 
+        SYSNAME_OID = ".1.3.6.1.2.1.1.5.0"
+        SYSDESC_OID = ".1.3.6.1.2.1.1.1.0"
+        snmp_sysname = (snmp_get_oid(a_device, oid=SYSNAME_OID))
+        SYSNAME = snmp_extract(snmp_sysname)
+        snmp_sysdesc = (snmp_get_oid(a_device, oid=SYSDESC_OID))
+        SYSDESC = snmp_extract(snmp_sysdesc)
+
+        if "Cisco" in SYSDESC:
             # Uptime when running config last changed    
             ccmHistoryRunningLastChangedoid = '1.3.6.1.4.1.9.9.43.1.1.1.0'   
             # Uptime when running config last saved (note any 'write' constitutes a save)
@@ -145,7 +166,9 @@ def lab3(list=None):
         
         
 
-                        
+
+
+
         
         
         
